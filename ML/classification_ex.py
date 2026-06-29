187e9506def30a543be7e9bdfe52d33b27f270d1
Kartik <sahukartikey171@gmail.com>
2026-06-29 19:44:08 +0530

    code

---
+++ ML/classification_ex.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+
+data = {
+    "hours":[1,2,3,4,5,6],
+    "pass":[0,0,0,1,1,1]
+}
+
+df = pd.DataFrame(data)
+X = df [["hours"]]
+Y = df [["pass"]]
+
+model = LogisticRegression()
+model.fit(X,Y)
+
+res = model.predict([[10]])
+prob = model.predict_proba([[3]])
+
+plt.scatter(df["hours"],df["pass"])
+plt.title("Student Graph !!!")
+plt.xlabel("Hours")
+plt.ylabel("Pass")
+plt.show()
